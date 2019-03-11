ee888cf2c6c6f0129b08a4e31a30199ae5f91352
Rick Russotto <russotto@ldeo.columbia.edu>
2019-03-11 16:37:25 -0400

    Reproduced BS03 plots, correct except winds
    Deleted files irrelevant to what I'm posting online.
    In the "ReproduceBrethertonSobelFigures" script,
    added code to reproduce all 7 figures in the paper.
    This works except u is way off in Figure 5.
    Now I realize there are errors with the u field as well in
    other figures, although these are small.
    (Did I forget to change back some test calculation? Look at
    the stuff in the other module again... see if error in Fig. 1
    winds shows up)
    
    	modified:   .ipynb_checkpoints/ReproduceBrethertonSobelFigures-checkpoint.ipynb
    	modified:   .ipynb_checkpoints/gill-checkpoint.py
    	deleted:    Figure_Production_7.ipynb
    	deleted:    GillForTRACMIP.ipynb
    	deleted:    GillForTRACMIP_v2.ipynb
    	deleted:    GillForTRACMIP_v3.ipynb
    	deleted:    GillModuleTester.ipynb
    	modified:   ReproduceBrethertonSobelFigures.ipynb
    	deleted:    Untitled.ipynb
    	modified:   __pycache__/gill.cpython-36.pyc
    	deleted:    figure_7_production.pdf
    	deleted:    figure_7_production.png
    	deleted:    figures/GillResponseBothPerturbationsLowerBranch.pdf
    	deleted:    figures/GillResponseBothPerturbationsLowerBranch.png
    	deleted:    figures/GillResponseOceanPerturbationLowerBranch.pdf
    	deleted:    figures/GillResponseOceanPerturbationLowerBranch.png
    	deleted:    figures/GillResponseOceanPerturbationUpperBranch.pdf
    	deleted:    figures/GillResponseOceanPerturbationUpperBranch.png
    	deleted:    figures/GillResponseOceanSmallSurfaceLowerBranch.pdf
    	deleted:    figures/GillResponseOceanSmallSurfaceLowerBranch.png
    	deleted:    figures/GillResponseSurfaceHeatingLowerBranch.pdf
    	deleted:    figures/GillResponseSurfaceHeatingLowerBranch.png
    	deleted:    figures/Gill_perturb_4_lobes.pdf
    	deleted:    figures/Gill_perturb_4_lobes.png
    	deleted:    figures/Gill_results_4_lobes_lower.pdf
    	deleted:    figures/Gill_results_4_lobes_lower.png
    	deleted:    figures/Gill_results_4_lobes_upper.pdf
    	deleted:    figures/Gill_results_4_lobes_upper.png
    	deleted:    figures/Gill_results_land_tripole_lower.pdf
    	deleted:    figures/Gill_results_land_tripole_lower.png
    	deleted:    figures/Gill_results_land_tripole_upper.pdf
    	deleted:    figures/Gill_results_land_tripole_upper.png
    	deleted:    figures/Gill_results_ocean_lower.pdf
    	deleted:    figures/Gill_results_ocean_lower.png
    	deleted:    figures/Gill_results_ocean_upper.pdf
    	deleted:    figures/Gill_results_ocean_upper.png
    	deleted:    gill.m
    	modified:   gill.py
    	deleted:    gill01.pdf
    	deleted:    gill01.png
    	deleted:    gill03.pdf
    	deleted:    gill03.png
    	deleted:    gillscratch.ipynb
    	new file:   plots/BS03_Figure_1.pdf
    	new file:   plots/BS03_Figure_1.png
    	new file:   plots/BS03_Figure_2.pdf
    	new file:   plots/BS03_Figure_2.png
    	new file:   plots/BS03_Figure_3.pdf
    	new file:   plots/BS03_Figure_3.png
    	new file:   plots/BS03_Figure_4.pdf
    	new file:   plots/BS03_Figure_4.png
    	new file:   plots/BS03_Figure_5.pdf
    	new file:   plots/BS03_Figure_5.png
    	new file:   plots/BS03_Figure_6.pdf
    	new file:   plots/BS03_Figure_6.png
    	new file:   plots/BS03_Figure_7.pdf
    	new file:   plots/BS03_Figure_7.png

--- .ipynb_checkpoints/gill-checkpoint.py
+++ .ipynb_checkpoints/gill-checkpoint.py
@@ -12,9 +12,17 @@
 # Rick Russotto
 # Started 17 October 2018
 
+# 8 March 2019: editing this for public posting, 
+# breaking backward compatibility with code for TRACMIP. 
+# Some changes: 
+# -passing only one dict argument into each function instead of 
+#  requiring separate arguments for M, Mhat, etc. 
+# -passing grid dimensions and other common parameters through each function for consistency
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse import spdiags
+from scipy.sparse import dia_matrix
 from scipy.sparse.linalg import spsolve
 
 
@@ -67,8 +75,8 @@ def setupGillM_Gaussian(nx=128, ny=120, lx=20, ly=20,
     M = D0*F*np.exp(-(Y-y0)*(Y-y0)/(2*sy*sy))
     M[0,:] = 0
     M[ny,:] = 0
-    if zonalcomp == 1: 
-        M = M - (np.mean(np.transpose(M)))*np.ones([1,nx])
+    if(zonalcomp): 
+        M = M - np.transpose(np.tile(np.mean(M, axis=1),[nx,1])) #Subtract zonal mean
         
     #Calculate derivatives and Fourier transform
     dMdy = -(Y-y0)*M/(sy*sy)
@@ -76,19 +84,20 @@ def setupGillM_Gaussian(nx=128, ny=120, lx=20, ly=20,
     Mhat = np.fft.fft(M)
     dMdyhat = np.fft.fft(dMdy)
     
-    #Return M, derivatives, Fourier transform:
+    #Return M, derivatives, Fourier transform, and the input grid parameters:
     returnDict = {
         'M': M,
         'dMdy': dMdy, 
         'd2Mdy2': d2Mdy2, 
         'Mhat': Mhat,
-        'dMdyhat': dMdyhat
+        'dMdyhat': dMdyhat,
+        'nx': nx,
+        'ny': ny,
+        'lx': lx,
+        'ly': ly
     }
     return returnDict
-    
-    
-def setupGillM_versatile():
-    print('Not implemented yet')
+
 
 
 
@@ -108,10 +117,10 @@ def setupGillM_versatile():
 # Function to do the computations for the Gill model
 #
 # Input variables:
-# M:         mass source as a function of x and y
-# dMdy:      meridional first derivative of mass source
-# dMdyhat:   Fourier transform of first derivative of mass source
-# nx, ny, lx, ly: see "setupGillM_Gaussian"
+# setupDict['M']:         mass source as a function of x and y
+# setupDict['Mhat']:      Fourier transform of mass source
+# setupDict['dMdyhat']:   Fourier transform of first derivative of mass source
+# nx, ny, lx, ly: see "setupGillM_Gaussian" (also in "setupDict")
 # H:         Layer depth
 # g:         gravity
 # beta:      variation of Coriolis parameter with y
@@ -125,11 +134,23 @@ def setupGillM_versatile():
 # 'phi':    geopotential
 # 'a':      nondimensonalized Rayleigh friction coefficient (should this be input instead?)
 # 'b':      thermal diffusivity, assumed == a
-def GillComputations(M, Mhat, dMdyhat, nx=128, ny=120, lx=20, ly=20, 
-                     H=1, g=1, beta=1, nodiss=0):
+def GillComputations(setupDict, H=1, g=1, beta=1, nodiss=0):
+    
+    #Extract variables from the setup dict
+    M = setupDict['M']
+    Mhat = setupDict['Mhat']
+    dMdyhat = setupDict['dMdyhat']
+    nx = setupDict['nx']
+    ny = setupDict['ny']
+    lx = setupDict['lx']
+    ly = setupDict['ly']
+                
+    
+    
     #Preliminary stuff:
     if(nodiss):
         a = 0.0001
+        #a = 0.00001 #Checking vorticity isn't scaled by this...
     else:
         a = 0.15   
         
@@ -161,6 +182,10 @@ def GillComputations(M, Mhat, dMdyhat, nx=128, ny=120, lx=20, ly=20,
 
     # %  This is done as a loop over wavenumbers, using only the interior 
     # %  y-gridpoints 2:ny (since v = 0 at the boundaries)
+    
+    #In frequency space, 
+    #The computation of the x derivative does not depend on the form of the 
+    #function in X. 
     
     #Note: arrays must be initialized as complex to avoid casting to reals
     vhat = np.zeros([ny+1, nx]) + 1j*np.zeros([ny+1,nx]) 
@@ -200,6 +225,7 @@ def GillComputations(M, Mhat, dMdyhat, nx=128, ny=120, lx=20, ly=20,
     
     #%  u calculated using div eqn: du/dx + dv/dy = M/H-b*phi/(c^2)
     dvdy = np.zeros([ny+1,nx]) #This one's supposed to be real
+    #dvdy = np.zeros([ny+1,nx]) +1j*np.zeros([ny+1,nx]) #this didn't help
     dvdy[1:ny,:] = (v[2:ny+1,:] - v[0:ny-1,:])/(2*dy)
 
     uhat = Mhat/H - b*phihat/(c*c)-np.fft.fft(dvdy)
@@ -225,6 +251,8 @@ def GillComputations(M, Mhat, dMdyhat, nx=128, ny=120, lx=20, ly=20,
     dphidx[:,0] = (phi[:,1]-phi[:,nx-1])/(2.*dx)
     dphidx[:,nx-1] = (phi[:,0] - phi[:,nx-2])/(2.*dx)
     u = (-1.*beta*Y*v+dphidx)/(-1.*a)
+    #u = (-1.*beta*Y*v+dphidx)/(-1.) #Trying this, didn't help
+    #Is this line the problem? u error seems to depend on division by a: 
 
     #OK, now need to return variables that might go into plots. 
     #In Figures 1 and 3 of the Matlab code, plots include:
@@ -232,6 +260,135 @@ def GillComputations(M, Mhat, dMdyhat, nx=128, ny=120, lx=20, ly=20,
     #zeta (vorticity)
     #u, v (wind velocity vector components)
     #phi: geopotential
+    #Also put grid specs in again
+    returnDict = {
+        'D': D, 
+        'zeta': zeta,
+        'u': u, 
+        'v': v,
+        'phi': phi,
+        'a': a,
+        'b': b,
+        'nx': nx,
+        'ny': ny,
+        'lx': lx,
+        'ly': ly
+    }
+    return returnDict
+
+
+
+#Similar function for WTG version (same inputs and outputs).
+#Requires zonal compensation of heat source 
+#(i.e. zonalcomp=1 in "setupGillM_Gaussian")
+def WTG_Computations(setupDict, H=1, g=1, beta=1, nodiss=0):
+
+    #Extract variables from the setup dict
+    M = setupDict['M']
+    Mhat = setupDict['Mhat']
+    dMdyhat = setupDict['dMdyhat']
+    nx = setupDict['nx']
+    ny = setupDict['ny']
+    lx = setupDict['lx']
+    ly = setupDict['ly']
+    
+    #Preliminary stuff:
+    if(nodiss):
+        a = 0.0001
+    else:
+        a = 0.15   
+        
+    b=a+0 #to avoid any possible "pointer" issues
+    
+    #Grid definitions
+    dx = lx/nx
+    x = -lx/2.+dx*np.arange(nx)
+    dy = ly/ny
+    y = -ly/2.+dy*np.arange(ny+1)
+    X,Y=np.meshgrid(x,y)
+    
+    #Shallow water phase speed
+    c = np.sqrt(g*H)
+    
+    #Define wavenumber matrix
+    kx = (2*np.pi/lx)*np.append(np.arange(nx/2), np.arange(-nx/2,0))
+    KX = np.ones([ny+1,1])*kx
+    
+    # %      Define v source term Sv = (a*d/dy - beta*y*d/dx)M/H
+    
+    Svhat = (a*dMdyhat - beta*1j*KX*Y*Mhat)/H
+
+    # %      Solve a*del^2 v + beta*dv/dx = Sv, or
+    # %  
+    # %      a*d2vhat/dy2 + (-a*k^2 + i*k*beta)vhat = Svhat
+    # %  
+    # %      This is done as a loop over wavenumbers, using only the interior 
+    # %      y-gridpoints 2:ny (since v = 0 at the boundaries)
+    # %      By skipping i = 1 (k=0) the solution automatically removes the zonal
+    # %      mean (k=0) contribution to the heating, as required by WTG.
+
+    vhat = np.zeros([ny+1,nx])+ 1j*np.zeros([ny+1,nx])
+    d1 = a/(dy*dy)
+    for i in np.arange(1,nx):
+        k = kx[i]
+        d0 = -2*d1 - a*k*k + 1j*k*beta
+        e = np.ones(ny-1)
+        diags = np.stack((d1*e, d0*e, d1*e))
+        Av = spdiags(diags, [-1, 0, 1], ny-1, ny-1)
+        r = Svhat[1:ny,i]
+        vhat[1:ny,i] = spsolve(Av,r)
+    v = np.real(np.fft.ifft(vhat))
+    D = M/H
+
+    # %      Calculate vorticity zeta from divergence and v:
+    # %
+    # %        a*zeta + beta*(y*D + v) = 0
+
+    zeta = (beta/a)*(-Y*D-v)
+
+    # %      u calculated using div eqn du/dx + dv/dy = M/H
+
+    dvdy = np.zeros([ny+1,nx])    
+    dvdy[1:ny,:] = (v[2:ny+1,:] - v[0:ny-1,:])/(2*dy)
+    uhat = Mhat/H - np.fft.fft(dvdy)    
+    uhat[:,1:nx] = uhat[:,1:nx]/(1j*KX[:,1:nx])
+    
+    #print(uhat)
+
+    # %      The k=0 components are indeterminate; for these go to zonally
+    # %      averaged (bar) vorticity equation dudybar = -zetabar 
+    # %      For WTG, ubar = 0 since no waveno 0 heat source.
+
+    uhat[:,0] = 0
+    #print(uhat)
+    #uhat[0,:] = 0 #try this, no, didn't work
+    u = np.real(np.fft.ifft(uhat))
+
+    #%      Calculate WTG phi from -del^2 phi = a*D - beta*y*zeta + beta*u
+    #%      We use the BC i*k*beta*y*phihat = a*dphihat/dy at the boundaries,
+    #%      derived from the momentum equations.
+
+    phihat = np.zeros([ny+1,nx]) + 1j*np.zeros([ny+1,nx])
+    yzetahat = np.fft.fft(Y*zeta)
+    Sphihat = a*Mhat/H - beta*yzetahat + beta*uhat
+    Sphihat[:,0] = 0
+
+    d1 = 1/(dy*dy)
+    for i in np.arange(1,nx):
+        k = kx[i]
+        e = np.ones(ny+1)
+        diags = np.stack((-d1*e, (2*d1+k*k)*e, -d1*e))
+        Ap = spdiags(diags, [-1, 0, 1], ny+1, ny+1)
+        Ap_dense = Ap.todense()
+        Ap_dense[0,1] = a/dy    
+        Ap_dense[0,0] = -a/dy - i*k*beta*(-ly/2)
+        Ap_dense[ny, ny] = a/dy - i*k*beta*(ly/2)
+        Ap_dense[ny, ny-1] = -a/dy
+        Ap_mod = dia_matrix(Ap_dense)
+        r = np.pad(Sphihat[1:ny,i], (1,1), 'constant', constant_values=(0, 0))
+        phihat[:,i] = spsolve(Ap_mod, r)
+    phi = np.real(np.fft.ifft(phihat))
+    
     
     returnDict = {
         'D': D, 
@@ -240,18 +397,40 @@ def GillComputations(M, Mhat, dMdyhat, nx=128, ny=120, lx=20, ly=20,
         'v': v,
         'phi': phi,
         'a': a,
-        'b': b
+        'b': b,
+        'nx': nx,
+        'ny': ny,
+        'lx': lx,
+        'ly': ly
     }
+    
     return returnDict
-    
-    #what else might I want?
-    
-#Functions to plot Gill divergence, vorticity, velocity and geopotential the way 
-#it's done in the Matlab code. 
-#More generally probably want to do this outside the module.
 
-#Need more arguments?
-def plotGillConvVortVel(D, zeta, u, v, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, stride=4, a=.15, nx=128, ny=120, lx=20, ly=20):
+    
+
+# Built-in plotting functions to streamline common use cases
+    
+# Functions to plot Gill divergence, vorticity, velocity and geopotential the way 
+# it's done in the Matlab code. 
+#
+# Input variables:
+# D, zeta, u, v, a: see "GillComputations" (obtain from "resultsDict")
+# xmin, xmax: x limits of plot
+# ymin, ymax: y limits of plot
+# stride: spacing between grid points where quiver arrows are plotted
+# nx, ny, lx, ly: see "setupGillM_Gaussian" (in "resultsDict")
+def plotGillDivVortVel(resultsDict, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, stride=4):
+    D = resultsDict['D']
+    zeta = resultsDict['zeta']
+    u = resultsDict['u']
+    v = resultsDict['v']
+    a = resultsDict['a']
+    b = resultsDict['b']
+    nx = resultsDict['nx']
+    ny = resultsDict['ny']
+    lx = resultsDict['lx']
+    ly = resultsDict['ly']
+    
     #Grid definitions
     dx = lx/nx
     x = -lx/2.+dx*np.arange(nx)
@@ -263,19 +442,27 @@ def plotGillConvVortVel(D, zeta, u, v, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, s
     plt.subplot(2,1,1)
     #%    Plot Gill divergence
     #3 sets of contours of different styles
+    #The way it's done in the paper, positive convergence (negative divergence) is solid. 
+    #Labeled correctly in Fig. 1 title but wrong in the caption.
+    #Actually plotting divergence but contour levels in the paper described as if it was convergence.
     cint = np.array([-0.9, -0.7, -0.5, -0.3, -0.1])
     plt.contour(x,y,D, levels=cint, colors='k', linestyles = 'solid')
-    cint = np.array([-0.06, -0.02])
+    cint = np.array([-0.06, -.02])
     plt.contour(x,y,D, levels=cint, colors='k', linestyles = 'dashed')
-    cint = np.array([0.02, 0.06, 0.1])
+    cint = np.array([.02, .06, .1])
     plt.contour(x,y,D, levels=cint, colors='k', linestyles = 'dashdot')
     plt.axis('equal')
     plt.axis([xmin, xmax, ymin, ymax])
-    #plt.xlabel('x/R$_{eq}$')
+    plt.gca().set_xticks(np.linspace(-10, 10, 11))
+    #plt.xlabel('x/R$_{eq}$') #Don't do this in top panel
     plt.ylabel('y/R$_{eq}$')
-    plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin, 
+    if a == b:
+        plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin, 
          'a = b = '+str(a)+ 'c/R$_{eq}$')
-    plt.title('Gill convergence')
+    else: #Note b different from a would require change to computation function
+        plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin,
+                'a = '+str(a) + 'c/R$_{eq}$; b = ' + str(b) + 'c/R$_{eq}$')
+    plt.title('Gill convergence (positive solid)') 
     
     plt.subplot(2,1,2)
     #%    czetamax = max(max(zeta))
@@ -285,6 +472,7 @@ def plotGillConvVortVel(D, zeta, u, v, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, s
     plt.contour(x,y,zeta, levels=np.flip(-1*cpos), colors='k', linestyles='dashed') #NumPy requires contours in increasing order
     plt.axis('equal')
     plt.axis([xmin, xmax, ymin, ymax])
+    plt.gca().set_xticks(np.linspace(-10, 10, 11))
     plt.xlabel('x/R$_{eq}$')
     plt.ylabel('y/R$_{eq}$')
     plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin, 
@@ -294,8 +482,14 @@ def plotGillConvVortVel(D, zeta, u, v, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, s
     #%    Plot velocity vectors
     plt.quiver(x[0:nx:stride], y[0:ny:stride], u[0:ny:stride,0:nx:stride], v[0:ny:stride,0:nx:stride], angles='xy', scale_units='xy', scale=2, color='b')
     
-def plotGillGeopotential(phi, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5,
-                        nx=128, ny=120, lx=20, ly=20, cphimax=2):
+    
+def plotGillGeopotential(resultsDict, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, cphimax=2):
+    phi = resultsDict['phi']
+    nx = resultsDict['nx']
+    ny = resultsDict['ny']
+    lx = resultsDict['lx']
+    ly = resultsDict['ly']
+    
     #Grid definitions
     dx = lx/nx
     x = -lx/2.+dx*np.arange(nx)
@@ -317,9 +511,110 @@ def plotGillGeopotential(phi, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5,
     plt.title('Gill geopotential')
     
     
+#Functions to plot the panels above with no new figure window, so they can be run inside subplots
+#subtitle: input argument for subplot title
+#Maybe should just use these functions for the entire test script.
+def plotDivergenceNoFigure(resultsDict, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, subtitle='Divergence'):
+    D = resultsDict['D']
+    a = resultsDict['a']
+    b = resultsDict['b']
+    nx = resultsDict['nx']
+    ny = resultsDict['ny']
+    lx = resultsDict['lx']
+    ly = resultsDict['ly']
     
-#Main function to do what Matlab code does, with defaults.
-#Can set this up with kwargs...
+    #Grid definitions
+    dx = lx/nx
+    x = -lx/2.+dx*np.arange(nx)
+    dy = ly/ny
+    y = -ly/2.+dy*np.arange(ny+1)
+    X,Y=np.meshgrid(x,y)
+    
+    cint = np.array([-0.9, -0.7, -0.5, -0.3, -0.1])
+    plt.contour(x,y,D, levels=cint, colors='k', linestyles = 'solid')
+    cint = np.array([-0.06, -.02])
+    plt.contour(x,y,D, levels=cint, colors='k', linestyles = 'dashed')
+    cint = np.array([.02, .06, .1])
+    plt.contour(x,y,D, levels=cint, colors='k', linestyles = 'dashdot')
+    plt.axis('equal')
+    plt.axis([xmin, xmax, ymin, ymax])
+    plt.gca().set_xticks(np.linspace(-10, 10, 11))
+    plt.xlabel('x/R$_{eq}$')
+    plt.ylabel('y/R$_{eq}$')
+    if a == b:
+        plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin, 
+         'a = b = '+str(a)+ 'c/R$_{eq}$')
+    else: 
+        plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin,
+                'a = '+str(a) + 'c/R$_{eq}$; b = ' + str(b) + 'c/R$_{eq}$')
+        
+    plt.title(subtitle) 
+    
+    
+def plotVortVelNoFigure(resultsDict, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, stride=4, subtitle='Velocity and Vorticity'):
+    zeta = resultsDict['zeta']
+    u = resultsDict['u']
+    v = resultsDict['v']
+    a = resultsDict['a']
+    b = resultsDict['b']
+    nx = resultsDict['nx']
+    ny = resultsDict['ny']
+    lx = resultsDict['lx']
+    ly = resultsDict['ly']
+    
+    #Grid definitions
+    dx = lx/nx
+    x = -lx/2.+dx*np.arange(nx)
+    dy = ly/ny
+    y = -ly/2.+dy*np.arange(ny+1)
+    X,Y=np.meshgrid(x,y)
+    
+    #Plot the vorticity (contours)
+    czetamax = 3 #fixed contours
+    cpos = np.array([0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9])*czetamax
+    plt.contour(x,y,zeta, levels=cpos, colors='k', linestyles='solid')
+    plt.contour(x,y,zeta, levels=np.flip(-1*cpos), colors='k', linestyles='dashed') #NumPy requires contours in increasing order
+    plt.axis('equal')
+    plt.axis([xmin, xmax, ymin, ymax])
+    plt.gca().set_xticks(np.linspace(-10, 10, 11))
+    plt.xlabel('x/R$_{eq}$')
+    plt.ylabel('y/R$_{eq}$')
+    plt.text(0.75*xmax + 0.25*xmin, 0.85*ymax + 0.15*ymin, 
+         'a = b = '+str(a)+ 'c/R$_{eq}$')
+
+    #%    Plot velocity vectors
+    plt.quiver(x[0:nx:stride], y[0:ny:stride], u[0:ny:stride,0:nx:stride], v[0:ny:stride,0:nx:stride], angles='xy', scale_units='xy', scale=2, color='b')
+    
+    plt.title(subtitle)
+    
+
+def plotGeopotentialNoFigure(resultsDict, xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, cphimax = 2, subtitle='Geopotential'):
+    phi = resultsDict['phi']
+    nx = resultsDict['nx']
+    ny = resultsDict['ny']
+    lx = resultsDict['lx']
+    ly = resultsDict['ly']
+    
+    #Grid definitions
+    dx = lx/nx
+    x = -lx/2.+dx*np.arange(nx)
+    dy = ly/ny
+    y = -ly/2.+dy*np.arange(ny+1)
+    X,Y=np.meshgrid(x,y)
+    
+    cpos = np.arange(0.1,2.1,0.2)*cphimax
+
+    plt.contour(x,y, phi, cpos, colors='k', linestyles='solid')
+    plt.contour(x,y, phi, np.flip(-1*cpos), colors='k', linestyles='dashed')
+    plt.axis('equal')
+    plt.axis([xmin, xmax, ymin, ymax])
+    plt.xlabel('x/R$_{eq}$')
+    plt.ylabel('y/R$_{eq}$')
+    
+    plt.title(subtitle)
+    
+    
+#A function to run everything with "default" settings
 def main_default(nx=128, ny=120, lx=20, ly=20, 
                  sx=2, sy=1, x0=0, y0=0, zonalcomp=0, H=1, g=1, beta=1, nodiss=0,
                  xmin=-10, xmax=10, ymin=-3.5, ymax=3.5, stride=4, cphimax=2, D0=1):
@@ -328,7 +623,7 @@ def main_default(nx=128, ny=120, lx=20, ly=20,
     dictM = setupGillM_Gaussian(nx=nx, ny=ny, lx=lx, ly=ly, 
                                 sx=sx, sy=sy, x0=x0, y0=y0, zonalcomp=zonalcomp, D0=D0)
     dictC = GillComputations(dictM['M'], dictM['Mhat'], dictM['dMdyhat'], 
-                             nx=128, ny=120, lx=20, ly=20, 
+                             nx=nx, ny=ny, lx=lx, ly=ly, 
                              H=H, g=g, beta=beta, nodiss=nodiss)
     #Run plotting functions
     plotGillConvVortVel(dictC['D'], dictC['zeta'], dictC['u'], dictC['v'], 
@@ -339,3 +634,4 @@ def main_default(nx=128, ny=120, lx=20, ly=20,
     
 
 #Version where sign of M is flipped?
+#No, just ability to specifcy D0 is sufficient
